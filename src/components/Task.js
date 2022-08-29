906a5e2395a4ed04efaa26d94cb6d377c7811aea
Stephanie Sison <sjsison27@icloud.com>
2022-08-29 15:32:06 -0700

    tests complete

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-function Task() {
+
+function Task({ text, category, onDeleteTask}) {
   
+  function handleClick() {
+    onDeleteTask(text)
+  }
 
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
 }
 
 export default Task;
+
+// I need to get data into here then take the div on line 8 and render it to TaskList.
